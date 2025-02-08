0130ec7d26057efa6e793f9141ff3a7ffba6b48e
Erick Koga <pessoal@erickkoga.com>
2025-02-07 22:12:05 -0300

    feat(web): ✨ integrate Tailwind CSS into the project with necessary dependencies and configuration

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig, loadEnv } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, "../../", "VITE_");
@@ -10,6 +11,6 @@ export default defineConfig(({ mode }) => {
     server: {
       port: port,
     },
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
   };
 });
